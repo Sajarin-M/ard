2112146407c6d61591aace8e38109bae76ee32b4
Sajarin M <sajarin@nodeshift.com>
2025-11-29 15:05:43 +0530

    change html title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,7 +23,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Find Time',
+        title: 'ARD',
       },
     ],
     links: [
